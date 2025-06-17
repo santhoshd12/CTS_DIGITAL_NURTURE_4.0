3536c7507e01752f11cf28e8d42b959ab488ad29
Santhosh D <santhoshdhananjeyan@gmail.com>
2025-06-17 22:16:49 +0530

    Reorganized project into src folder, cleaned up old files

--- Assignments/Week1/Singleton/Logger.java
+++ Assignments/Week1/Singleton/Logger.java
@@ -1,4 +1,4 @@
-package Singleton;
+package Assignments.Week1.Singleton;
 public class Logger {
     private static Logger instance;
 
--- Assignments/Week1/Singleton/Main.java
+++ Assignments/Week1/Singleton/Main.java
@@ -1,4 +1,4 @@
-package Singleton;
+package Assignments.Week1.Singleton;
 
 public class Main {
     public static void main(String[] args) {
